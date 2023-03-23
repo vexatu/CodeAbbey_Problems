95f4b8e9384035fe9e41fc5f1ccdcbf6b566fcaa
David Jipa <126858419+vexatu@users.noreply.github.com>
2023-03-23 18:51:40 +0200

    Update Problem4_CodeAbbey.py

--- Problem4_CodeAbbey.py
+++ Problem4_CodeAbbey.py
@@ -2,8 +2,8 @@ liste = int(input("Nr de perechi: "))
 new_list = []
 for i in range(1, liste+1):
     numbers = input().split()
-    new_list.append(min(int(numbers[0]), int(numbers[1])))
-print(new_list)
+    new_list.append(str(min(int(numbers[0]), int(numbers[1]))))
+print(" ".join(new_list))
 
 
 
